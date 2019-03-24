ada3c9c48e07ac043e24cbf765f0abbed5bd7cd8
Robert Heaton <bobjheaton@gmail.com>
2019-03-24 09:36:53 -0700

    [OSAB] Add basic scatterplot test

---
+++ test.py
@@ -0,0 +1,44 @@
+from bashplotlib.scatterplot import build_scatter
+
+if __name__ == "__main__":
+    # Build a scatter plot
+    scatter = build_scatter(
+            [-10,20,30],
+            [-10,20,30],
+            10,
+            'x',
+            'default',
+            'test hello')
+
+    # This is what we know our scatter plot should
+    # look like. We have to be careful that indentations
+    # match.
+    expected_scatter = """----------------------------
+|        test hello        |
+----------------------------
+----------------------------
+|                       x  |
+|                          |
+|                          |
+|                   x      |
+|                          |
+|                          |
+|                          |
+|                          |
+|                          |
+|                          |
+|                          |
+| x                        |
+----------------------------"""
+
+    # Compare what we got to what we expected, and
+    # fail with a helpful error message if anything is
+    # different.
+    if expected_scatter == scatter:
+        print("SUCCESS!")
+    else:
+        print("FAILURE!")
+        print("Expected:")
+        print(expected_scatter)
+        print("Got:")
+        print(scatter)
